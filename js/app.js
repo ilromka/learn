311bc99dd3fe9ecc416bed9faa886480081c8ea8
ilromka <ilromka@mail.ru>
2015-01-03 19:25:09 +0300

    add all files

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('tutorial', [ ]);
+
+app.config(["$locationProvider", function($locationProvider) {
+    $locationProvider.html5Mode(true);
+}]);
+
+app.controller("defaultCtrl", ["$scope", function($scope) {
+    $scope.sayHello = function (name) {
+        alert("Привет, " + name);
+    }
+	this.data = {
+		'name' : 'defaultCtrl'
+	};
+}]);
